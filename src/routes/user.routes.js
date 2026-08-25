ef49a6fe526d27dab5c30974a9993c295eae4db2
Shubham Sharma <shubhamsh3099@gmail.com>
2026-08-25 15:26:08 +0530

    Add authorize to user get endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,10 +10,11 @@ import {
 import { asyncHandler } from "../middleware/asyncHandler.js";
 
 import { authenticate } from "../middleware/auth.middleware.js";
+import { authorize } from "../middleware/authorize.middleware.js";
 
 const router = express.Router();
 
-router.get("/", authenticate, asyncHandler(getUsers));
+router.get("/", authenticate, authorize("ADMIN"), asyncHandler(getUsers));
 router.get("/:id", authenticate, asyncHandler(getUserById));
 router.patch("/:id", authenticate, asyncHandler(updateUser));
 router.delete("/:id", authenticate, asyncHandler(deleteUser));
