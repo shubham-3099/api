c58ef82b47d5efb8796f7eb1eb0306831118d6d5
Shubham Sharma <shubhamsh3099@gmail.com>
2026-08-25 15:21:50 +0530

    Add auth middleware in user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,11 +9,13 @@ import {
 
 import { asyncHandler } from "../middleware/asyncHandler.js";
 
+import { authenticate } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
-router.get("/", asyncHandler(getUsers));
-router.get("/:id", asyncHandler(getUserById));
-router.patch("/:id", asyncHandler(updateUser));
-router.delete("/:id", asyncHandler(deleteUser));
+router.get("/", authenticate, asyncHandler(getUsers));
+router.get("/:id", authenticate, asyncHandler(getUserById));
+router.patch("/:id", authenticate, asyncHandler(updateUser));
+router.delete("/:id", authenticate, asyncHandler(deleteUser));
 
 export default router;
